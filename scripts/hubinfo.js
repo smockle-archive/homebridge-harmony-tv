eca90f63161b415629e32c430c754c1f557f3a68
Clay Miller <clay@smockle.com>
2019-01-28 20:33:06 -0800

    Rewrite without Promises

--- scripts/hubinfo.js
+++ scripts/hubinfo.js
@@ -9,53 +9,56 @@ const deviceId = process.argv.slice(2)[2];
 // Print help and exit
 if (!host && !remoteId && !deviceId) {
   console.log(`hubinfo
-Lists unique identifier for a Harmony Hub, connected devices and supported commands
-
-Usage:
-  $ hubinfo host [remoteId] [deviceId]
-
-Examples:
-  $ hubinfo 192.168.1.10
-  $ hubinfo 192.168.1.10 22571993
-  $ hubinfo 192.168.1.10 22571993 72306838`);
+  Lists unique identifier for a Harmony Hub, connected devices and supported commands
+  
+  Usage:
+    $ hubinfo host [remoteId] [deviceId]
+  
+  Examples:
+    $ hubinfo 192.168.1.10
+    $ hubinfo 192.168.1.10 22571993
+    $ hubinfo 192.168.1.10 22571993 72306838`);
   process.exit(0);
 }
 
 // Print remoteId and exit
 if (host && !remoteId && !deviceId) {
-  getRemoteId(host)
-    .then(remoteId => {
+  (async () => {
+    try {
+      const remoteId = await getRemoteId(host);
       console.log(remoteId);
       process.exit(0);
-    })
-    .catch(error => {
+    } catch (error) {
       console.error(error);
       process.exit(1);
-    });
+    }
+  })();
 }
 
 // Print device list and exit
 if (host && remoteId && !deviceId) {
-  getDevices(host, remoteId)
-    .then(devices => {
+  (async () => {
+    try {
+      const devices = await getDevices(host, remoteId);
       devices.forEach(device => console.log(device));
       process.exit(0);
-    })
-    .catch(error => {
+    } catch (error) {
       console.error(error);
       process.exit(1);
-    });
+    }
+  })();
 }
 
 // Print commands for given device and exit
 if (host && remoteId && deviceId) {
-  getCommands(host, remoteId, deviceId)
-    .then(commands => {
+  (async () => {
+    try {
+      const commands = await getCommands(host, remoteId, deviceId);
       commands.forEach(command => console.log(command));
       process.exit(0);
-    })
-    .catch(error => {
+    } catch (error) {
       console.error(error);
       process.exit(1);
-    });
+    }
+  })();
 }
